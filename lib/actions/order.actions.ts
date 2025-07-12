0d5cf81928645440b6871bd318ba637c310d5c33
timishot <timidotcom58@gmail.com>
2025-07-13 00:28:23 +0100

    fix

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -29,8 +29,8 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
                 buyerId: order.buyerId || "",
             },
             mode: 'payment',
-            success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
-            cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+            success_url: `https://eventup-frontend.vercel.app/profile`,
+            cancel_url: `https://eventup-frontend.vercel.app/`,
         });
 
         redirect(session.url!);
