114c4d6764cd0de1372226639cc9196b24f39cda
timishot <timidotcom58@gmail.com>
2025-07-26 10:36:01 +0100

    push

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -17,10 +17,12 @@ const MobileNav = () => {
                <SheetTrigger className="align-middle">
                    <Image src="/assets/icon/menu.svg" alt="menu" width={24} height={24} className="cursor-pointer" />
                </SheetTrigger>
-               <SheetContent className="flex flex-col ml-5 gap-6 bg-white md:hidden">
+               <SheetContent className="flex flex-col  gap-6 bg-white md:hidden">
                    <Image src="/assets/icon/logo.png" alt="logo"  width={128} height={38}/>
                    <Separator className="border border-gray-50 "/>
-                   <NavItems />
+                   <div className="ml-5">
+                       <NavItems />
+                   </div>
                </SheetContent>
            </Sheet>
        </nav>
